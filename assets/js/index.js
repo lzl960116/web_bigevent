4c3660f26bcabd167c4bc4f05198918fec606ab1
LZL <403288582@qq.com.com>
2020-10-08 15:48:57 +0800

    首页完成

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function () {
+    getUserInfo()
+    var layer = layui.layer;
+    $('#btn-tuichu').on('click',function () {
+        layer.confirm('是否确定退出?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            localStorage.removeItem('token')
+            location.href="/login.html"
+            layer.close(index);
+          });
+    })
+})
+
+// 获取用户信息
+function getUserInfo() {
+    $.ajax({
+        url:'/my/userinfo',
+        // headers:{
+        //     Authorization:localStorage.getItem('token') ||""
+        // },
+        success:function (res) {
+            if (res.status !==0) {
+                return layui.layer.msg('获取失败')
+            }
+            //渲染用户头像
+            renderAvatar(res.data)
+        },
+    //    complete:function(res){
+    //         if(res.responseJSON.status === 1 && res.responseJSON.message ==='身份认证失败！'){
+    //             localStorage.removeItem('token')
+    //             location.href='/login.html'
+    //         }
+    //     }
+    })
+}
+//渲染用戶頭像
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎啊！'+name);
+
+    if (user.user_pic !==null) {
+       $('.layui-nav-img').show().attr('src',user.user_pic);
+       $('.text-avatar').hide()
+    }else{
+        $('.layui-nav-img').hide();
+        var text = name[0].toUpperCase();
+        $('.text-avatar').show().html(text);
+    }
+}
